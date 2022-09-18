664c606b0ed2f3e2e96e74c533ba9b5ceab14480
Zeu Capua <zeucapua@gmail.com>
2022-09-18 15:44:09 -0700

    added fonts, texture, remove navbar

--- src/components/BlogHeader.svelte
+++ src/components/BlogHeader.svelte
@@ -5,10 +5,10 @@
 
 <div class="flex flex-col w-full h-fit not-prose border-b-2 pt-8 mb-8">
   <div>
-    <p class="text-4xl md:text-5xl text-white font-black">{title}</p>
+    <p class="text-4xl md:text-5xl text-zaccent font-['Montra'] tracking-wide">{title}</p>
   </div>
-  <div class="flex py-3 flex-row gap-8 font-bold">
-    <p class="text-primary">by Zeu Capua</p>
-    <p class="text-secondary">{publishDate}</p>
+  <div class="flex py-3 flex-row gap-8 font-bold font-['Quicksand']">
+    <p class="text-zprimary">by Zeu Capua</p>
+    <p class="text-zsecondary">{publishDate}</p>
   </div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,13 @@
-const { primary, secondary } = require('daisyui/src/colors');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			zprimary: "#44adee",
+			zsecondary: "#e9498c",
+			zaccent: "#fff133"
+		}
 	},
 	plugins: [require('@tailwindcss/typography'), require("daisyui")],
 }
