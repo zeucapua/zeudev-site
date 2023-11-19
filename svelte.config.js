07182914d01133dbc6caadd30afd828efafbbc31
Zeu Capua <zeucapua@gmail.com>
2023-11-18 16:31:15 -0800

    change to vercel (fix)?

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,9 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+      runtime: 'nodejs18.x' 
+    })
 	},
 
 };
