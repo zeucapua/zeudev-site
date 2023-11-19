6e4b8650c0bbb0ee53d3271ec4cd33d4bbf390c9
Zeu Capua <zeucapua@gmail.com>
2023-11-18 16:23:55 -0800

    completed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import "../app.css";
 </script>
 
-<main class="flex flex-col w-full h-full min-w-screen min-h-screen p-8 gap-8 bg-yellow-500 font-general">
+<main class="flex flex-col w-full h-full min-w-screen min-h-screen p-8 gap-12 bg-yellow-500 font-general">
   <slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { experience } from "$lib/information";
   import Icon from "@iconify/svelte";
+  import { experience, explorations, technologies } from "$lib/information";
 </script>
 
 <svelte:head>
@@ -8,7 +8,7 @@
 </svelte:head>
 
 <!-- quick links -->
-<section class="flex flex-col gap-4 mx-auto justify-center items-center">
+<section class="flex flex-col gap-4 mx-auto justify-center items-center border border-2 p-6 lg:p-8 rounded-xl border-black border-dashed">
   <h3 class="text-3xl lg:text-5xl font-bold">
     <Icon icon="pixelarticons:external-link" class="inline" />
     quick links
@@ -30,7 +30,7 @@
 <!-- hero -->
 <section class="flex flex-col gap-4">
   <h1 class="text-7xl lg:text-9xl font-bold items-center">
-    <Icon icon="pixelarticons:moon-stars" class="inline border border-4 border-black p-4 rounded-xl hover:-rotate-12 transition-all duration-300 shadow-xl" />
+    <Icon icon="pixelarticons:moon-stars" class="inline border border-4 border-black p-4 rounded-xl hover:bg-yellow-400 hover:-rotate-12 transition-all duration-300 shadow-xl" />
     zeu capua
   </h1>
   <h2 class="text-4xl lg:text-7xl font-bold font-quicksand">
@@ -45,17 +45,58 @@
     <img 
       src="/content-files-pencil-ruler-3.svg" 
       alt="Pencil cup from Streamline HQ"
-      class="inline w-32 border border-4 border-black p-4 rounded-xl hover:-rotate-12 transition-all duration-300 shadow-xl"/>
+      class="inline w-20 lg:w-32 border border-4 border-black p-4 rounded-xl hover:-rotate-12 hover:bg-yellow-400 transition-all duration-300 shadow-xl"/>
     experience
   </h3>
-  {#each experience as exp}
-    <p>{exp.title}</p>
-  {/each}
+
+  <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 overflow-x-auto">
+    {#each experience as exp}
+      <a href={exp.link} class="flex flex-col gap-4 border border-2 border-dashed border-black rounded-xl p-4 w-full max-w-2xl justify-between hover:bg-yellow-400 transition-all duration-300">
+        <div>
+          <h4 class="text-3xl font-black">{exp.title}</h4>
+          <p class="text-xl italic font-normal">{exp.organization}</p>
+        </div>
+
+        <p class="font-bold font-quicksand">{exp.description}</p>
+        <div class="flex gap-4">
+          {#each exp.tech as tech}
+            <!-- change to component with link to tech -->
+            <Icon icon={technologies[tech]} class="text-5xl border border-black border-dashed border-2 rounded-full p-2" />
+          {/each}
+        </div>
+      </a>
+    {/each}
+  </div>
 </section>
 
 
 <!-- explorations -->
-
+<section class="flex flex-col gap-4">
+  <h3 class="text-3xl lg:text-5xl font-bold">
+    <img 
+      src="/coding-apps-websites-dinosaur-error-4.svg" 
+      alt="Dinosaur Error 4 from Streamline HQ"
+      class="inline w-20 lg:w-32 border border-4 border-black p-4 rounded-xl hover:-rotate-12 hover:bg-yellow-400 transition-all duration-300 shadow-xl"
+    />
+    explorations
+  </h3>
+  <div class="grid grid-cols-1 lg:grid-cols-4 gap-4">
+    {#each explorations as exp}
+      <a href={exp.link} class="flex flex-col justify-between gap-2 p-4 border border-2 border-dashed border-black rounded-xl hover:bg-yellow-400 transition-all duration-300 shadow-lg">
+        <h5 class="text-lg font-black">{exp.title}</h5>
+        <p class="font-quicksand">{exp.description}</p>
+        <div class="flex justify-between">
+          <div class="flex gap-2">
+            {#each exp.tech as tech}
+              <Icon icon={technologies[tech]} class="text-3xl border border-black border-dashed rounded-full p-1" />
+            {/each}
+          </div> 
+          <Icon icon={"pixelarticons:external-link"} class="self-end" />
+        </div>
+      </a>
+    {/each}
+  </div>
+</section>
 
 
 <!-- testimonials -->
@@ -63,14 +104,50 @@
 
 
 <!-- contact me -->
-
-
-
-<!-- credits -->
-<section>
-  <p>Art by Streamline HQ</p>
-  <p>Logos from Iconify (various packs)</p>
+<section class="flex flex-col gap-4">
+  <h3 class="text-3xl lg:text-5xl font-bold">
+    <img 
+      src="/coding-apps-websites-conference-2.svg" 
+      alt="Conference 2 from Streamline HQ"
+      class="inline w-20 lg:w-32 border border-4 border-black p-4 rounded-xl hover:-rotate-12 hover:bg-yellow-400 transition-all duration-300 shadow-xl"
+    />
+    information desk
+  </h3>
+  <a href="/Capua-Resume.pdf" download class="flex gap-4 items-center w-fit border border-2 border-black border-dashed p-4 rounded-xl hover:bg-yellow-400 transition-all duration-300 shadow-lg">
+    <img
+      src="/content-files-pdf-2.svg"
+      alt="PDF 2 from Streamline HQ"
+      class="w-16"
+    />
+    <p class="flex flex-col text-xl font-black font-quicksand">
+      Resume 
+      <span class="text-sm">(last updated: 11-18-2023)</span>
+    </p>
+  </a>
 </section>
 
 
-
+<!-- credits -->
+<section class="flex flex-col mx-auto items-center gap-2">
+  <div class="flex gap-4">
+    <img 
+      src="/pet-animals-bear.svg" 
+      alt="Pet Bear from Streamline HQ"
+      class="w-16" 
+    />
+    <img 
+      src="/pet-animals-buffalo-1.svg" 
+      alt="Pet Buffalo from Streamline HQ"
+      class="w-16" 
+    />
+    <img 
+      src="/pet-animals-gorilla-2.svg" 
+      alt="Pet Gorilla from Streamline HQ"
+      class="w-16" 
+    />
+  </div>
+  <div class="flex flex-col items-center font-quicksand">
+    <p>Art by Streamline HQ</p>
+    <p>Logos from Iconify (various packs)</p>
+  </div>
+</section>
